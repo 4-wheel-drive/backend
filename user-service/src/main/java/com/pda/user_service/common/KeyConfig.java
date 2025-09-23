5abb04c91d812a7e0bbbbc1ced9702fa3acfe835
임채연 <dlacodus0407@gmail.com>
2025-09-24 07:45:53 +0900

    feat(auth) : 토큰 생성과 검증 로직을 구현한다

---
+++ user-service/src/main/java/com/pda/user_service/common/KeyConfig.java
@@ -0,0 +1,22 @@
+package com.pda.user_service.common;
+
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import javax.crypto.SecretKey;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class KeyConfig {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Bean
+    public SecretKey secretKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
+}
